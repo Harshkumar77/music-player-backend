2f289883d5e01f80e68510e88ad20229c93a4230
mighty_volcano <72347597+Harshkumar77@users.noreply.github.com>
2022-04-12 12:16:59 +0530

    port from env

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
-const express = require("express");
-const YoutubeMusicApi = require("youtube-music-api");
-const ytdl = require("ytdl-core");
+const express = require("express")
+const YoutubeMusicApi = require("youtube-music-api")
+const ytdl = require("ytdl-core")
 
-const ytApi = new YoutubeMusicApi();
-ytApi.initalize();
+const ytApi = new YoutubeMusicApi()
+ytApi.initalize()
 
-const app = express();
-const port = 5000;
+const app = express()
+const port = process.env.PORT
 
-var bodyParser = require("body-parser");
-const cors = require("cors");
+var bodyParser = require("body-parser")
+const cors = require("cors")
 const corsOptions = {
   origin: "*",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
-app.use(express.static(__dirname + "/build"));
+}
+app.use(cors(corsOptions))
+app.use(express.static(__dirname + "/build"))
 
-app.get("/", (req, res) => res.sendFile(__dirname + "/build/index.html"));
+app.get("/", (req, res) => res.sendFile(__dirname + "/build/index.html"))
 
-app.get("/api", (_, res) => res.send("Yay api is running"));
+app.get("/api", (_, res) => res.send("Yay api is running"))
 
 app.get("/api/search", async (req, res) => {
-  const searchQuery = req.query.q;
-  res.json((await ytApi.search(searchQuery, "song")).content.slice(0, 6));
-});
+  const searchQuery = req.query.q
+  res.json((await ytApi.search(searchQuery, "song")).content.slice(0, 6))
+})
 
 app.get("/api/getMusic", async (req, res) => {
-  const videoId = req.query.v;
+  const videoId = req.query.v
   res.redirect(
     (
       await ytdl.getInfo("https://www.youtube.com/watch?v=" + videoId)
     ).formats.filter((_) => _.hasVideo === false)[0].url
-  );
-});
+  )
+})
 
-app.get("*", (req, res) => res.sendFile(__dirname + "/build/index.html"));
+app.get("*", (req, res) => res.sendFile(__dirname + "/build/index.html"))
 
-app.listen(port, () => console.log(`Server started at port ${port}`));
+app.listen(port, () => console.log(`Server started at port ${port}`))
