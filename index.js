8f260b4907e3bb1853fe0c7f8bd8952c3127acf7
mighty_volcano <72347597+Harshkumar77@users.noreply.github.com>
2022-04-25 09:27:11 +0530

    converted common js to module

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-require("dotenv").config()
-const express = require("express")
-const YoutubeMusicApi = require("youtube-music-api")
-const ytdl = require("ytdl-core")
-const fs = require("node:fs")
+import "dotenv/config"
+import express from "express"
+import YoutubeMusicApi from "youtube-music-api"
+import ytdl from "ytdl-core"
 
 const ytApi = new YoutubeMusicApi()
 ytApi.initalize()
@@ -10,17 +9,9 @@ ytApi.initalize()
 const app = express()
 const port = process.env.PORT
 
-var bodyParser = require("body-parser")
-const cors = require("cors")
-const corsOptions = {
-  origin: "*",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-}
-app.use(cors(corsOptions))
-app.use(express.static(__dirname + "/build"))
+app.use(express.static("./build"))
 
-app.get("/", (req, res) => res.sendFile(__dirname + "/build/index.html"))
+app.get("/", (req, res) => res.sendFile("./build/index.html"))
 
 app.get("/api", (_, res) => res.send("Yay api is running"))
 
